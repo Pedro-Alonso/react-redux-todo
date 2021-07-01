f5815ddfc9d6bfad9dd3b18272880f4bf0b317a8
Pedro-Alonso <pedro.alonsod.1986@outlook.com>
2021-07-01 10:13:57 +0200

    update the codes

--- api/server.js
+++ api/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const colors = require('colors');
 const cors = require('cors');
 const { json } = require('body-parser');
 const { nanoid } = require('nanoid');
-const colors = require('colors');
+
+dotenv.config({ path: './config.env' });
 
 const app = express();
-dotenv.config({ path: './config.env' });
+
 app.use(cors());
 app.use(json());
 
@@ -14,7 +16,7 @@ let todos = [
 	{
 		id: nanoid(),
 		title: 'todo 1',
-		completed: true
+		completed: true,
 	},
 	{
 		id: nanoid(),
@@ -35,38 +37,37 @@ let todos = [
 		id: nanoid(),
 		title: 'todo 5',
 		completed: false,
-	}
+	},
 ];
 
 app.get('/todos', (req, res) => res.send(todos));
 
 app.post('/todos', (req, res) => {
-	const todo = { id: nanoid(), title: req.body.title, completed: false };
+	const todo = { title: req.body.title, id: nanoid(), completed: false };
 	todos.push(todo);
-	
 	return res.send(todo);
 });
 
-app.patch('todos/:id', (req, res) => {
+app.patch('/todos/:id', (req, res) => {
 	const id = req.params.id;
 	const index = todos.findIndex((todo) => todo.id == id);
 	const completed = Boolean(req.body.completed);
 	if (index > -1) {
 		todos[index].completed = completed;
 	}
-
 	return res.send(todos[index]);
 });
 
-app.delete('todos/:id', (req, res) => {
+app.delete('/todos/:id', (req, res) => {
 	const id = req.params.id;
 	const index = todos.findIndex((todo) => todo.id == id);
 	if (index > -1) {
 		todos.splice(index, 1);
 	}
 
-	res.send(todos);
+	return res.send(todos);
 });
 
 const PORT = 8000;
+
 app.listen(PORT, console.log(`Server running on port ${PORT}`.green.bold));
